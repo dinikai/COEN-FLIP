0335c585dcbfb8ba7c334234ef39e6e679f036fa
dinikai <dinikaigd@gmail.com>
2022-04-24 12:35:35 +0700

    Добавьте файлы проекта.

---
+++ Assets/Scripts/CoinFlip.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinFlip : MonoBehaviour
+{
+    [SerializeField] private Transform coinTransform;
+    [SerializeField] private AudioSource coinSFX, coinMusic;
+    [SerializeField] private AudioClip orelClip, reshkaClip;
+    [SerializeField] private Animator animator;
+    public bool isFlipping = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            isFlipping = !isFlipping;
+
+            if (isFlipping)
+            {
+                animator.SetBool("IsFlipping", true);
+                animator.Play("CoinFlip", 0, 0);
+                coinMusic.Play();
+            } else
+            {
+                animator.SetBool("IsFlipping", false);
+                float coinSide = Random.Range(0, 101);
+                Debug.Log(coinSide);
+                if(coinSide > FakeChances.ReshkaChances)
+                {
+                    coinTransform.rotation = Quaternion.Euler(0, coinTransform.rotation.y, coinTransform.rotation.z);
+                    coinSFX.clip = orelClip;
+                } else
+                {
+                    coinTransform.rotation = Quaternion.Euler(180, coinTransform.rotation.y, coinTransform.rotation.z);
+                    coinSFX.clip = reshkaClip;
+                }
+                coinSFX.Play();
+                coinMusic.Stop();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/FakeChances.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FakeChances : MonoBehaviour
+{
+    [SerializeField] private Slider reshkaSlider;
+    public static float ReshkaChances = 50;
+
+    public void GoToChances()
+    {
+        SceneManager.LoadScene("Chances");
+    }
+
+    public void SetChances()
+    {
+        ReshkaChances = reshkaSlider.value;
+        SceneManager.LoadScene("Coin");
+    }
+}
